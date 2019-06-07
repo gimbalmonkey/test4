63a4f8a2039770caced8fe3a76440933dfeba350
gimbalmonkey <bryan.begay@oregonstate.edu>
2019-06-07 13:54:38 -0700

    initial commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2477571,
+    "boundingBox": {
+        "lx": 474647.9,
+        "ly": 4942101.7700000009,
+        "lz": 170.07,
+        "ux": 475294.78,
+        "uy": 4942748.65,
+        "uz": 816.9500000000046
+    },
+    "tightBoundingBox": {
+        "lx": 474647.9,
+        "ly": 4942101.7700000009,
+        "lz": 170.07,
+        "ux": 475294.78,
+        "uy": 4942744.41,
+        "uz": 273.76
+    },
+    "pointAttributes": "LAS",
+    "spacing": 5.602145195007324,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
